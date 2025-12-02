b0b83ed8e24b8852a2a482785cd04bd0a19ba9ad
p-wn <93124806+p-wn@users.noreply.github.com>
2025-12-02 12:59:52 +0900

    lfs setting

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -5,7 +5,6 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("push test");
     }
 
     // Update is called once per frame
